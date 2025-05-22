0c27827e1919430a01a7f9cf12752248cfa5988e
NoahDD-01 <noahdd216@gmail.com>
2025-05-22 15:55:00 +0630

    update_5_22

--- myapp/models.py
+++ myapp/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class HearArea(models.Model):
+    icon_img = models.ImageField(upload_to='hero_area', blank=True, null=True)
+    icon_title = models.CharField(max_length=20,blank=True,null=True)
+    bg_img =  models.ImageField(upload_to='hero_area', blank=True, null=True)
+    title = models.CharField(max_length=20,blank=True,null=True)
+    note = models.CharField(max_length=100, blank=True,null=True)
+    video = models.URLField(null=True,blank=True)
